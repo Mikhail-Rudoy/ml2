52eb180815c1f695e63fb2b9fb6e1cd0d5901e6d
Mikhail Rudoy <mrudoy@gmail.com>
2012-02-16 20:04:26 -0500

    second excersise finished

---
+++ hw-3/make_abba.py
@@ -0,0 +1,8 @@
+def make_abba(a, b):
+    return a + 2 * b + a
+
+print make_abba("Hi", "Bye")
+print make_abba("Yo", "Alice")
+print make_abba("x", "y")
+print make_abba("x", "")
+print make_abba("", "y")
